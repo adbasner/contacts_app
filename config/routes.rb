daca063aa448e0a7308f46c742ab46f01a60ea8a
Andrew Basner <andrewbasner@gmail.com>
2018-06-28 17:31:02 -0400

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-  get "/contact_info_url" => "api/contacts#get_first_contact"
-  get "/all_contacts_url" => "api/contacts#get_all_contacts"
+  namespace :api do
+    get "/contact_info_url" => "contacts#get_first_contact"
+    get "/all_contacts_url" => "contacts#get_all_contacts"
+  end
 end
