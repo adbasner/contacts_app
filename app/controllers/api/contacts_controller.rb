d888d5ecfd8ca6dc70a2d3db7f800efee9682395
Andrew Basner <andrewbasner@gmail.com>
2018-07-09 22:04:04 -0400

    add middle name to db and forms

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -18,6 +18,7 @@ class Api::ContactsController < ApplicationController
     #
     @contact1 = Contact.new(
       first_name: params[:input_first_name], # input_first_name
+      middle_name: params[:input_middle_name],
       last_name: params[:input_last_name], # input_last_name
       email: params[:input_email],# input_email
       phone_number: params[:input_phone_number]# input_phone_number
@@ -34,6 +35,7 @@ class Api::ContactsController < ApplicationController
     # update
     # = input data or original
     @contact1.first_name = params[:input_first_name] || @contact1.first_name
+    @contact1.middle_name = params[:input_middle_name] || @contact1.middle_name
     @contact1.last_name = params[:input_last_name] || @contact1.last_name
     @contact1.email = params[:input_email] || @contact1.email
     @contact1.phone_number = params[:input_phone_number] || @contact1.phone_number
--- app/views/api/contacts/index.json.jbuilder
+++ app/views/api/contacts/index.json.jbuilder
@@ -2,6 +2,7 @@ json.array! @contacts.each do |contact|
 
   json.id contact.id
   json.first_name contact.first_name
+  json.middle_name contact.middle_name
   json.last_name contact.last_name
   json.email contact.email
   json.phone_number contact.phone_number
